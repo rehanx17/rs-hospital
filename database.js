48ebf25bd109d5a68fbab7fc7270d6881fd3215b
rehanx17 <shaikhrehan8050@gmail.com>
2025-04-26 13:35:54 +0530

    Update database.js

--- database.js
+++ database.js
@@ -1,18 +1,18 @@
-const sqlite3 = require("sqlite3").verbose();
+const Database = require('better-sqlite3');
+const db = new Database('appointments.db');
 const db = new sqlite3.Database("appointments.db");
 
-db.serialize(() => {
-  db.run(`
-    CREATE TABLE IF NOT EXISTS appointments (
-      id INTEGER PRIMARY KEY AUTOINCREMENT,
-      name TEXT,
-      email TEXT,
-      phone TEXT,
-      date TEXT,
-      time TEXT,
-      message TEXT
-    )
-  `);
-});
+db.prepare(`
+  CREATE TABLE IF NOT EXISTS appointments (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT,
+    email TEXT,
+    phone TEXT,
+    date TEXT,
+    time TEXT,
+    message TEXT
+  )
+`).run();
+
 
 module.exports = db;
